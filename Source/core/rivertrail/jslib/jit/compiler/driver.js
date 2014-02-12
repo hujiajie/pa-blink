d194091c3115d503e4cca7f55883e04af3e29635
hujiajie <hujiajie.org@gmail.com>
2014-02-12 13:55:04 +0800

    Update driver.js.
    Legacy ParallelArray literals are removed. External variables will be handled in another way, so for the time being, we do not consider any extra argument.

--- Source/core/rivertrail/jslib/jit/compiler/driver.js
+++ Source/core/rivertrail/jslib/jit/compiler/driver.js
@@ -45,14 +45,9 @@ RiverTrail.compiler = (function () {
     // This is the compiler driver proper. 
     
     // The ast is opaque at this point so the Narcissus constants aren't needed.
-    var inferPAType = RiverTrail.Helper.inferPAType;
     
     // whether to use kernel caching or not
     var useKernelCaching = true;
-    // whether to cache OpenCL buffers
-    var useBufferCaching = true;
-
-    var suppressOpenCL = false;
 
     var openCLContext; 
     var dpoInterface;
@@ -96,53 +91,29 @@ RiverTrail.compiler = (function () {
     }
 
     // main hook to start the compilation/execution process for running a construct using OpenCL
-    // paSource -> 'this' inside kernel
+    // source -> the source holding the elements
     // f -> function to run
-    // construct -> [combine|map|comprehension|comprehensionScalar]
-    // rankOrShape -> either rank of iterationspace or in case of comprehension and comprehensionScalar the shape of the iterationspace
-    // args -> additional arguments to the kernel
-    var compileAndGo = function compileAndGo (paSource, f, construct, rankOrShape, args, enable64BitFloatingPoint) {
-        var paResult = null;
+    // construct -> 'mapPar' only so far
+    var compileAndGo = function compileAndGo (source, f, construct, enable64BitFloatingPoint) {
+        var result = null;
         var kernelString;
         var lowPrecision;
         var ast;
-        var i;
-        // If f is a low_precision wrapped function, unwrap it first
-        if (f instanceof low_precision.wrapper) {
-            lowPrecision = true;
-            f = f.unwrap();
-        } else {
-            lowPrecision = !enable64BitFloatingPoint;
-        }
-        var defaultNumberType = lowPrecision ? "float": "double";
+        lowPrecision = !enable64BitFloatingPoint;
 
-        // First convert all array arguments into suitable flat representations that can be passed to
+        // First convert the source into suitable flat representations that can be passed to
         // the OpenCL side
 
-        args = Array.prototype.map.call(args, 
-                                     function (object) {
-                                         if (object instanceof Array) {
-                                             if ((typeof(openCLContext.canBeMapped) === 'function') && (openCLContext.canBeMapped(object))) {
-                                                 // we have found a JavaScript array that can be directly mapped, so we keep it
-                                                 return object;
-                                             } else {
-                                                 return new RiverTrail.Helper.FlatArray( lowPrecision ? Float32Array : Float64Array, object);
-                                             }
-                                         } else {
-                                             return object;
-                                         }});
-
-        var argumentTypes = constructArgTypes(construct, args, rankOrShape, defaultNumberType);
+        var flatSource = new RiverTrail.Helper.FlatArray( lowPrecision ? Float32Array : Float64Array, source);
 
         if (f.openCLCache !== undefined) {
             if (useKernelCaching) {
-                var cacheEntry = getCacheEntry(f, construct, paSource, argumentTypes, lowPrecision, rankOrShape);
+                var cacheEntry = getCacheEntry(f, construct, flatSource, lowPrecision);
                 // try and find a matching kernel from previous runs
                 if (cacheEntry != null) {
-                    paResult = RiverTrail.compiler.runOCL(paSource, cacheEntry.kernel, cacheEntry.ast, f, 
-                                      construct, rankOrShape, args, argumentTypes, lowPrecision, 
-                                      enable64BitFloatingPoint, useBufferCaching, useKernelCaching);
-                    return paResult;
+                    result = RiverTrail.compiler.runOCL(flatSource, cacheEntry.kernel, cacheEntry.ast, f, 
+                                      construct, lowPrecision, enable64BitFloatingPoint, useKernelCaching);
+                    return result;
                 }
             } else {
                 // remove cache 
@@ -151,7 +122,6 @@ RiverTrail.compiler = (function () {
         } 
         //
         // NOTE: we only get here if caching has failed!
-        // We need to pass in argumentTypes.
         //
         if (useKernelCaching && (f.openCLCache === undefined)) {
             // create empty cache
@@ -159,46 +129,38 @@ RiverTrail.compiler = (function () {
         }
                         
         try {
-            ast = parse(paSource, construct, rankOrShape, f.toString(), args, lowPrecision); // parse, no code gen
-            kernelString = RiverTrail.compiler.codeGen.compile(ast, paSource, rankOrShape, construct); // Creates an OpenCL kernel function
+            ast = parse(flatSource, construct, f.toString(), lowPrecision); // parse, no code gen
+            kernelString = RiverTrail.compiler.codeGen.compile(ast, construct); // Creates an OpenCL kernel function
         } catch (e) {
             RiverTrail.Helper.debugThrow(e);
         }
         
         if (RiverTrail.compiler.verboseDebug) {    
             console.log("::parseGenRunOCL:kernelString: ", kernelString);
-            console.log("::parseGenRunOCL:args: ", JSON.stringify(args));
         }
 
-        if (suppressOpenCL) {
-            console.log("Not executing OpenCL returning 'this' parseGenRunOCL:surpressOpenCL: ", suppressOpenCL);
-            paResult = this;
-        } else {
-            // what arg should be used, args seems correct.?
+        try {
+            result = RiverTrail.compiler.runOCL(flatSource, kernelString, ast, f, construct,
+                                            lowPrecision, enable64BitFloatingPoint, useKernelCaching);
+        } catch (e) {
             try {
-                paResult = RiverTrail.compiler.runOCL(paSource, kernelString, ast, f, construct, rankOrShape, args, 
-                                                argumentTypes, lowPrecision, enable64BitFloatingPoint, useBufferCaching, useKernelCaching);
-            } catch (e) {
-                try {
-                    RiverTrail.Helper.debugThrow(e + RiverTrail.compiler.openCLContext.buildLog);
-                } catch (e2) {
-                    RiverTrail.Helper.debugThrow(e); // ignore e2. If buildlog throws, there simply is none.
-                }
+                RiverTrail.Helper.debugThrow(e + RiverTrail.compiler.openCLContext.buildLog);
+            } catch (e2) {
+                RiverTrail.Helper.debugThrow(e); // ignore e2. If buildlog throws, there simply is none.
             }
         }
         // NOTE: Do not add general code here. This is not the only exit from this function!
-        return paResult;
+        return result;
     };
 
     //
     // Driver method to steer compilation process
     //
-    function parse(paSource, construct, rankOrShape, kernel, args, lowPrecision) {
+    function parse(source, construct, kernel, lowPrecision) {
         var ast = RiverTrail.Helper.parseFunction(kernel);
-        var rank = rankOrShape.length || rankOrShape;
         try {
-            RiverTrail.Typeinference.analyze(ast, paSource, construct, rank, args, lowPrecision);
-            RiverTrail.RangeAnalysis.analyze(ast, paSource, construct, rankOrShape, args);
+            RiverTrail.Typeinference.analyze(ast, source, construct, lowPrecision);
+            RiverTrail.RangeAnalysis.analyze(ast, source, construct);
             RiverTrail.RangeAnalysis.propagate(ast, construct);
             RiverTrail.InferBlockFlow.infer(ast);
             RiverTrail.InferMem.infer(ast);
@@ -208,15 +170,7 @@ RiverTrail.compiler = (function () {
         return ast;
     }
     
-    var getCacheEntry = function (f, construct, paSource, argumentTypes, lowPrecision, rankOrShape) {
-        var argumentMatches = function (argTypeA, argTypeB) {
-            return ((argTypeA.inferredType === argTypeB.inferredType) &&
-                    equalsShape(argTypeA.dimSize, argTypeB.dimSize));
-        };
-        var argumentsMatch = function (argTypesA, argTypesB) {
-            if (argTypesA.length !== argTypesB.length) return false;
-            return argTypesA.every(function (eA, idx) { return argumentMatches(eA, argTypesB[idx]); });
-        };
+    var getCacheEntry = function (f, construct, source, lowPrecision) {
         var i;
         var entry;
         // try and find a matching kernel from previous runs
@@ -225,8 +179,8 @@ RiverTrail.compiler = (function () {
             if ((construct === entry.construct) &&
                 (lowPrecision === entry.lowPrecision) &&
                 (entry.source === f) &&
-                argumentsMatch(argumentTypes, entry.argumentTypes) &&
-                (((construct !== "comprehension") && (construct !== "comprehensionScalar") && argumentMatches(RiverTrail.Helper.inferPAType(paSource), entry.paType)) || equalsShape(rankOrShape, entry.iterSpace))
+                (source.inferredType === entry.sourceType.inferredType) &&
+                (equalsShape(source.dimSize, entry.sourceType.dimSize))
                ) {
                 return f.openCLCache[i];
             }
@@ -234,59 +188,6 @@ RiverTrail.compiler = (function () {
         return null;
     };
 
-    var constructArgTypes = function (construct, args, rankOrShape, defaultNumberType) {
-        var argumentTypes = [];
-        var i;
-        if (construct == "combine") {
-            // the kernel is called with an index as first argument, which has type int [rankOrShape]
-            // and an extra isIndex attribute to differentiate it from the rest
-            argumentTypes.push({ inferredType: "int", dimSize: [rankOrShape], attributes: { isIndex: true} });
-        } else if (construct == "comprehension") {
-            // the kernel is called with an index as first argument, which has type int[rankOrShape.length]
-            // and an extra isIndex attribute to differentiate it from the rest
-            argumentTypes.push({ inferredType: "int", dimSize: [rankOrShape.length], attributes: { isIndex: true} });
-        } else if (construct == "comprehensionScalar") {
-            // the kernel is called with an index as first argument, which has type int
-            // and an extra isIndex attribute to differentiate it from the rest
-            argumentTypes.push({ inferredType: "int", dimSize: [], attributes: { isIndex: true} });
-        }
-
-        //
-        // Push the types for all other arguments
-        // This simple iterates through the array in order, pushing the types.
-        //
-
-        for (i = 0; i < args.length; i++) {
-            var argument = args[i];
-            if (argument instanceof ParallelArray) {
-                argumentTypes.push(inferPAType(argument));
-            } else if (argument instanceof RiverTrail.Helper.FlatArray) {
-                argumentTypes.push({ inferredType: defaultNumberType, dimSize: argument.shape});
-            } else if (argument instanceof Array) {
-                // SAH: if an array makes it here without being transformed into a flat array, it
-                //      must be a dense, homogeneous JavaScript array. Those are always double arrays
-                //      and we assume the shape can be derived by looking at the first element in
-                //      each dimension.
-                // NOTE: I use /* jsval */ double as type to make sure these are not confused with 
-                //       ordinary arrays when checking for matching signatures.
-                argumentTypes.push({ inferredType: "/* jsval */ double", dimSize: function makeShape(a, r) { if (a instanceof Array) { r.push(a.length); makeShape(a[0], r); } return r;}(argument, []) });
-            } else if (RiverTrail.Helper.isTypedArray(argument)) {
-                argumentTypes.push({ inferredType: RiverTrail.Helper.inferTypedArrayType(argument), dimSize: [argument.length] });
-            } else if (argument instanceof RiverTrail.Helper.Integer) {
-                // These are special integer values used for offsets and the like. 
-                argumentTypes.push({ inferredType: "int", dimSize: [] });
-            } else if (typeof (argument) === "number") {
-                // scalar values are treated as floats
-                argumentTypes.push({ inferredType: defaultNumberType, dimSize: [] });
-            } else if (argument instanceof Number) {
-                // numbers are floats
-                argumentTypes.push({ inferredType: defaultNumberType, dimSize: [] });
-            } else {
-                throw new Error("Type derivation for argument not implemented yet");
-            }
-        }
-        return argumentTypes;
-    };
     function astTypeConverter (key, value) {
         if (key === 'type' && (typeof value === 'number') ) { 
             if (opTypeNames[tokens[value]]) {
@@ -321,7 +222,6 @@ RiverTrail.compiler = (function () {
                 Array.prototype.every.call(shapeA, function (a,idx) { return a == shapeB[idx];}));
     };
     
-// end code from parallel array
     return {
         verboseDebug: false,
         debug: false,
